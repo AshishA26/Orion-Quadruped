7b18989c7bb2f9c4d5a698447f86ccb62008d2fa
Ashish Agrahari <shinyflame007@gmail.com>
2025-12-10 22:29:02 -0500

    Test code and fixed mold model

---
+++ Test_Code/Servo_test/Servo_test.ino
@@ -0,0 +1,63 @@
+// This program moves the leg up and down in a straight vertical fashion
+
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// 0 to 270 degrees is 500 to 2500 microseconds
+#define SERVO_TOP_MIN 550 
+#define SERVO_TOP_MAX 1100 
+#define SERVO_BOTTOM_MIN 550
+#define SERVO_BOTTOM_MAX 2500
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("16 channel Servo test!");
+  pwm.begin();
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
+}
+
+void loop() {
+  // Set all servos to start
+  pwm.writeMicroseconds(0, 900);
+  pwm.writeMicroseconds(1, 1150);
+  delay(2000);
+  
+  // Sweep all servos from 0 to 180 degrees
+  float top_microsec = 900;
+  float bot_microsec = 1150;
+  for(float i = 0; i <= 350; i += 1) {
+      top_microsec-=1;
+      bot_microsec+=1;
+      pwm.writeMicroseconds(0, int(top_microsec));
+      pwm.writeMicroseconds(1, int(bot_microsec));
+      // Serial.print("Top ms: ");
+      // Serial.print(top_microsec);
+      // Serial.print("  Bottom ms: ");
+      // Serial.print(bot_microsec);
+      // Serial.println();
+      delay(1);
+  }
+  for(float i = 0; i <= 350; i += 1) {
+      top_microsec+=1;
+      bot_microsec-=1;
+      pwm.writeMicroseconds(0, int(top_microsec));
+      pwm.writeMicroseconds(1, int(bot_microsec));
+      // Serial.print("Top ms: ");
+      // Serial.print(top_microsec);
+      // Serial.print("  Bottom ms: ");
+      // Serial.print(bot_microsec);
+      // Serial.println();
+      delay(1);
+  }
+}
+
+// int angleToPulse(int ang) {
+//   int pulse = map(ang, 0, 270, SERVOMIN, SERVOMAX);
+//   Serial.print("Angle: "); Serial.print(ang);
+//   Serial.print(" pulse: "); Serial.println(pulse);
+//   return pulse;
+// }
