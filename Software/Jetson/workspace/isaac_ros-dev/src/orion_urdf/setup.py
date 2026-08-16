2b4c1e695727c6da4ca4dec8abdd1f63dd9bee21
Ashish Agrahari <shinyflame007@gmail.com>
2026-08-16 17:09:34 -0400

    Add urdf

---
+++ Software/Jetson/workspace/isaac_ros-dev/src/orion_urdf/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'orion_urdf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+z        ('share/' + package_name + '/meshes', glob('meshes/*')), #include meshes
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='orion',
+    maintainer_email='orion@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
