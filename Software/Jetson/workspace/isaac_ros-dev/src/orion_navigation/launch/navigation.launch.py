82ea70ee8eb18f3d04d9f76bc3f50ba730d84908
Ashish Agrahari <shinyflame007@gmail.com>
2026-08-16 20:34:11 -0400

    Add costmap and path planner, commented out urdf changes for now

--- Software/Jetson/workspace/isaac_ros-dev/src/orion_navigation/launch/navigation.launch.py
+++ Software/Jetson/workspace/isaac_ros-dev/src/orion_navigation/launch/navigation.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
@@ -10,5 +11,19 @@ def generate_launch_description():
     orion_navigation_dir = get_package_share_directory('orion_navigation')
     params_file = os.path.join(orion_navigation_dir, 'config', 'params.yaml')
 
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+
+    navigation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_bringup_dir, 'launch', 'navigation_launch.py')
+        ),
+        launch_arguments={
+            'params_file': params_file,
+            'use_sim_time': 'false',
+            'autostart': 'true',
+        }.items()
+    )
+
     return LaunchDescription([
+        navigation_launch
     ])
