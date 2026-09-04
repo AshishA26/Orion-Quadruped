fb846719dee065fb46cf1d718f59d891f41b81d8
Ashish Agrahari <shinyflame007@gmail.com>
2026-09-04 19:51:15 -0400

    Add nav2 pure pursuit for comparison

--- Software/Jetson/workspace/isaac_ros-dev/src/orion_navigation/launch/navigation.launch.py
+++ Software/Jetson/workspace/isaac_ros-dev/src/orion_navigation/launch/navigation.launch.py
@@ -11,8 +11,14 @@ def generate_launch_description():
     orion_navigation_dir = get_package_share_directory('orion_navigation')
     params_file = os.path.join(orion_navigation_dir, 'config', 'params.yaml')
 
-    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    use_custom_pure_pursuit = LaunchConfiguration('use_custom_pure_pursuit')
+    declare_use_custom_pp_cmd = DeclareLaunchArgument(
+        'use_custom_pure_pursuit',
+        default_value='true',
+        description='Whether to use the custom pure pursuit node or Nav2 Regulated Pure Pursuit'
+    )
 
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     navigation_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(nav2_bringup_dir, 'launch', 'navigation_launch.py')
@@ -24,19 +30,33 @@ def generate_launch_description():
         }.items()
     )
 
+    # Custom Pure Pursuit Node
     pure_pursuit_node = Node(
         package='orion_navigation',
         executable='pure_pursuit_node',
         name='pure_pursuit_node',
         parameters=[params_file],
         output='screen',
+        condition=IfCondition(use_custom_pure_pursuit),
         remappings=[
             ('plan', '/plan'),                        # Nav2 planned path (from planner_server)
             ('nav_motion_cmd', '/nav_motion_cmd'),    # Consumed by cmd_mux_node
         ],
     )
+    
+    # Velocity Smoother / cmd_vel remapper for Nav2 Pure Pursuit
+    # Nav2 outputs to cmd_vel. We want it to publish to nav_motion_cmd
+    remap_cmd_vel_node = Node(
+        package='topic_tools',
+        executable='relay',
+        name='cmd_vel_relay',
+        arguments=['cmd_vel', 'nav_motion_cmd'],
+        condition=UnlessCondition(use_custom_pure_pursuit)
+    )
 
     return LaunchDescription([
+        declare_use_custom_pp_cmd,
         navigation_launch,
         pure_pursuit_node,
+        remap_cmd_vel_node
     ])
